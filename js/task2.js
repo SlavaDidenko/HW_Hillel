d1fd0a6926b8e0eb14a6a277599625b3bf02c91c
Viacheslav <105516738+SlavaDidenko@users.noreply.github.com>
2022-07-22 16:03:11 +0300

    Update task2.js

--- js/task2.js
+++ js/task2.js
@@ -1,8 +1,24 @@
 function pow(x, n) {
-  if (n === 1) {
+  if (n == 1) {
     return x;
+
+  } else if (n < 0) {
+
+    return 1 / pow(x, - n);
   } else {
+
     return x * pow(x, n - 1);
   }
 }
-console.log(pow(3, 3));
+
+function cur(a) {
+  return function(x) {
+    return function(n) {
+      return a(x, n);
+    };
+  };
+}
+
+let curPow = cur(pow);
+
+console.log(curPow(3)(3));
